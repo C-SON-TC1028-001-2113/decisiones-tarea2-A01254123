9829c475de4da422fcf10c4c2da60a11c48c78ba
A01254123 <A01254123@itesm.mx>
2021-08-21 19:56:33 +0000

    Ejercicio licencia_A01254123CesarMtz

--- assignments/02Licencia/src/exercise.py
+++ assignments/02Licencia/src/exercise.py
@@ -1,8 +1,20 @@
 def main():
     #escribe tu código abajo de esta línea
-    
-
-
+    edad = int(input("Ingresa tu edad: "))
+    if edad>0:
+        if edad>=18:
+            identificacion = str(input("¿Tienes identificación oficial? (s/n): "))
+            if identificacion=='s'or identificacion=='n':
+                if edad>=18 and identificacion== 's':
+                    print("Trámite de licencia concedido")
+                else:
+                    print("No cumples requisitos")
+            else:
+                print("Respuesta incorrecta")
+        else:
+            print("No cumples requisitos")
+    else:
+        print("Respuesta incorrecta")    
 if __name__=='__main__':
     main()
 
