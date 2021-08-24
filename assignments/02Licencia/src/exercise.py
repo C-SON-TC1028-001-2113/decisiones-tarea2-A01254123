80a16d1d84f2687c0f241c48ddfe3c49cea1b4b2
A01254123 <A01254123@itesm.mx>
2021-08-24 17:52:40 +0000

    decisiones-tarea2-A01254123cesarmtz

--- assignments/02Licencia/src/exercise.py
+++ assignments/02Licencia/src/exercise.py
@@ -3,7 +3,7 @@ def main():
     edad = int(input("Ingresa tu edad: "))
     if edad>0:
         if edad>=18:
-            identificacion = str(input("¿Tienes identificación oficial? (s/n): "))
+            identificacion=str(input("¿Tienes identificación oficial? (s/n): "))
             if identificacion=='s'or identificacion=='n':
                 if edad>=18 and identificacion== 's':
                     print("Trámite de licencia concedido")
